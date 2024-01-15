f1aa6ed00d492d7e832b74b072ddc5a71529721d
mrroot <baisalkalykov69@gmail.com>
2024-01-15 23:29:41 +0600

    movie

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
+const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+const apiUrl = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const apiSearchUrl=  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+getMovie2(apiUrl);
 
-const API_KEY = "6167421d-7e3d-4e06-83e4-c7cd0b113afa";
-const apiUrl = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1";
-const apiSearchUrl= "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
-getMovie(apiUrl);
-
-async function getMovie(url) {
+async function getMovie2(url) {
     const resp = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
@@ -12,28 +11,32 @@ async function getMovie(url) {
         },
     });
     const respData = await resp.json();
-    console.log(respData);
-    showMovies(respData); 
+    console.log(respData)
+    showMovies2(respData);
 }
+showMovies2(apiUrl)
+function showMovies2(data) {
+    const moviesEl = document.querySelector(".movies");
+    moviesEl.innerHTML = ""; // Очищаем предыдущие фильмы
 
-function showMovies(data) {
-    const movieSel = document.querySelector(".movies");
-    data.items.forEach(movie => {
+    data.films.forEach(movie => {
         const movieEl = document.createElement('div');
         movieEl.classList.add("movie-card");
         movieEl.innerHTML = `
-            <div class="movie-card-inner">
-               <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="movie-card__img">
-               <div class="movie-card-darkened"></div>
-            </div>
-            <div class="movie__info">
-               <p class="movie__title">${movie.nameRu}</p>
-               <p class="movie__category">${movie.genres.map(genre => `${genre.genre}`).join(', ')}</p>
-               <div class="movie__average movie__average__green">9</div>
-            </div>
+        <div class="movie-card-inner">
+        <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="movie-card__img">
+        <div class="movie-card-darkened"></div>
+    </div>
+    <div class="movie__info">
+        <p class="movie__title">${movie.nameRu}</p>
+        <p class="movie__category">${movie.genres.map(genre => genre.genre).join(', ')}</p>
+        <div class="movie__average movie__average__green">9</div>
+    </div>
         `;
-        movieSel.appendChild(movieEl);
+        moviesEl.appendChild(movieEl);
+        
     });
+    if(data){}
 }
 
 const form = document.querySelector(".header__form")
@@ -43,6 +46,7 @@ form.addEventListener("submit", (e)=>{
     e.preventDefault();
     const apiSearch = `${apiSearchUrl}${search.value}`
     if(search.value){
-        getMovie(apiSearch)
+        getMovie2(apiSearch)
+        search.value = ""
     }
 })
